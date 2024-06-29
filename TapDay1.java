28c525f97aa0074bbc8a34374a042bc55975ceec
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2024-06-29 22:39:41 +0530

    Add files via upload

---
+++ TapDay1.java
@@ -0,0 +1,30 @@
+/*
+ * Write a program that checks whether a given integer value n ends with 0.
+
+Example:
+Input: 1230
+Output: Yes
+
+Your Task:
+Develop a program to check whether a given integer n ends with 0, and share your solution in the comments below! Don't forget to use the hashtag #30DaysCodeAtTAPAcademy.
+
+ */
+
+import java.util.Scanner;
+public class TapDay1
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num % 10 == 0)
+        {
+            System.out.println("Yes "+num+" Ends with Zero");
+        }
+        else
+        {
+            System.out.println("No "+num+" Not end  with Zero");
+        }
+        
+    }
+    
+}
